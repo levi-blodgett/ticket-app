d8c523aed9ad46d54f4a1eb15a3d698ef0137fc8
levi-blodgett <levi_blodgett@outlook.com>
2023-02-28 15:08:54 -0700

    Created component header and pages, nav works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <div>My App</div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
